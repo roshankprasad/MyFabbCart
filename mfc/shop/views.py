c5154a9213e16a43386773b254d329279e66203a
Roshan <roshanpucsd@gmail.com>
2019-10-20 20:14:16 +0530

    first commit

---
+++ mfc/shop/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("We are at about")
+
+def contact(request):
+    return HttpResponse("we are at contact")
+def tracker(request):
+    return HttpResponse("we are at tracker")
+
+def search(request):
+    return HttpResponse("we are at search")
+
+def productview(request):
+    return HttpResponse("we are at productview")
+def checkout(request):
+    return HttpResponse("we are at checkout")
